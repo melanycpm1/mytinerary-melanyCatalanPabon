4c6c6d8ec9e3b15072ff7201e245ebcdd6e73346
melanycpm1 <melanycatalanpabon@gmail.com>
2024-03-20 11:04:24 -0300

    estilo temporal de city

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import getCityByName from "../services/cities";
-
+import Header from "./Header"
 function City() {
     const params = useParams();
     const [ciudad, setCiudad] = useState({});
@@ -16,25 +16,23 @@ function City() {
     
     return (
         <>
-            <h1 className="text-lg md:text-xl lg:text-2xl xl:ext-3xl text-center my-10">Detalles</h1>
+            <h1 className="font-serif text-xl md:text-3xl lg:text-5xl xl:text-7xl text-center my-10">Detalles -{ciudad.name}</h1>
             <section>
-                <h2 className="text-sm md:text-base lg:text-lg xl:text-xl text-center my-10">{ciudad.name}</h2>
-                <p>{ciudad.description}</p>
-                <img src={ciudad.image} alt={ciudad.name}/>
+                <p className="text-center text-xs md:text-sm lg:text-base xl:text-lg font-serif my-10">{ciudad.description}</p>
+                <img src={ciudad.image} alt={ciudad.name} className="w-4/5 h-96 m-auto"/>
                 {ciudad.itineraries && ciudad.itineraries.map((itinerary, index) => (
-                    <div key={index}>
+                    <div key={index} className="text-center bg-cyan-900 my-44 py-20">
                         <h3>{itinerary.title}</h3>
-                        <img src={itinerary.activities[0]} alt={itinerary.title} />
-                        <img src={itinerary.guide_image} alt={itinerary.guide}/>
+                        <img src={itinerary.guide_image} alt={itinerary.guide} className="m-auto  border-solid border-y-blue-50 border-2"/>
                         <p>Guía: {itinerary.guide}</p>
                         <p>Precio: {itinerary.price}</p>
                         <p>Duración: {itinerary.duration} horas</p>
                         <p>Descripción: {itinerary.description}</p>
                         <p>Actividades:</p>
-                        <ul>
+                        <ul className="flex justify-center">
                             {itinerary.activities.map((activity, idx) => (
                                 <li key={idx}>
-                                    <img src={activity} alt={`Actividad ${idx + 1}`} />
+                                    <img src={activity} alt={`Actividad ${idx + 1}`} className="w-5/6 h-60"/>
                                 </li>
                             ))}
                         </ul>
